6832f18cd92a94f89e52eab2a32ad923c9a74d63
Alonso Diaz <lcoderl@hotmail.com>
2019-10-26 16:10:43 -0700

    working on homework 5

--- python/homework 5.py
+++ python/homework 5.py
@@ -1,5 +1,46 @@
-from numbers import  Number
+from numbers import Number
 import random
+
+
+def compute(e, varval={}):
+    if isinstance(e, Number):
+        return e
+    elif isinstance(e, str):
+        v = varval.get(e)
+        # If we find a value for e, we return it; otherwise we return e.
+        return e if v is None else v
+    else:
+        op, l, r = e
+        # We simplify the left and right subexpressions first.
+        ll = compute(l, varval=varval)
+        rr = compute(r, varval=varval)
+        # And we carry out the operation if we can.
+        if isinstance(ll, Number) and isinstance(rr, Number):
+            if op == '+':
+                return ll + rr
+            elif op == '-':
+                return ll - rr
+            elif op == '*':
+                return ll * rr
+            elif op == '/' and rr != 0:
+                return ll / rr
+        # Not simplifiable.
+        return op, ll, rr
+
+
+def variable_substitution(e, d):
+    """Performs variable substitutions in e according to the replacement dictionary d"""
+    if isinstance(e, tuple):
+        op, e1, e2 = e
+        ee1 = variable_substitution(e1, d)
+        ee2 = variable_substitution(e2, d)
+        return op, ee1, ee2
+    elif isinstance(e, Number):
+        return e
+    else:
+        # We perform the substitution, if one is specified.
+        return d.get(e, e)
+
 
 def derivate(e, x):
     """Returns the derivative of e wrt x.
@@ -21,6 +62,7 @@ def derivate(e, x):
 
 # Exercise: implementation of value equality
 def variables(e, p=[]):
+    x = p
     if isinstance(e, tuple):
         _, l, r = e
         if isinstance(l, str):
@@ -28,10 +70,12 @@ def variables(e, p=[]):
         if isinstance(r, str):
             p.append(r)
         if isinstance(l, tuple):
-            variables(l, p)
+            variables(l, x)
         if isinstance(r, tuple):
-            variables(r, p)
-    return set(p)
+            variables(r, x)
+    elif isinstance(e, str):
+        p.append(e)
+    return set(x)
 
 
 def value_equality(e1, e2, num_samples=1000, tolerance=1e-6):
@@ -42,12 +86,16 @@ def value_equality(e1, e2, num_samples=1000, tolerance=1e-6):
     the values of the two expressions have to be closer than tolerance.
     It can be done in less than 10 lines of code."""
     # YOUR CODE HERE
-    dank = 0
-    for i in range(1000):
-        if isinstance(e1, tuple) and isinstance(e2, tuple):
-            op, l, r = e1
-
-
+    d = variables(e1)
+    c = variables(e2)
+    # print(d)
+    print(c)
+    # for i in range(num_samples):
+    #     r = random.randint(0, num_samples)
+    #     var1 = {element: r for element in variables(e1)}
+    #     var2 = {element: r for element in variables(e2)}
+    #     if abs(compute(e1, var1)-compute(e2, var2)) > tolerance: return False
+    # return True
 
 
 # print(derivate(('+', 'x', 'x'), 'x'))
@@ -67,14 +115,14 @@ def value_equality(e1, e2, num_samples=1000, tolerance=1e-6):
 
 e1 = ('+', ('*', 'x', 1), ('*', 'y', 0))
 e2 = 'x'
-print(value_equality(e1, e2))
+# print(value_equality(e1, e2))
 # true
 
 e3 = ('/', ('*', 'x', 'x'), ('*', 'x', 1))
 print(value_equality(e1, e3))
 # true
 e4 = ('/', 'y', 2)
-print(value_equality(e1, e4))
+# print(value_equality(e1, e4))
 # false
-print(value_equality(e3, e4))
+# print(value_equality(e3, e4))
 # false
